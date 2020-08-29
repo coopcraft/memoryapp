9355d2e4a91cb35be435db4d9ab38c1099548ca7
Scott <scottc520@gmail.com>
2020-08-29 07:56:16 -0600

    Add navigation bar

--- corememapp/urls.py
+++ corememapp/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    path('index/', views.memory_list, name='index'),
+    path('memory/<int:pk>/', views.memory_detail, name='memory_detail'),
     path('klc/', views.klc, name='klc'),
     path('rjc/', views.rjc, name='rjc'),
+    path('lac/', views.lac, name='lac'),
 ]
--- corememapp/views.py
+++ corememapp/views.py
@@ -1,19 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from corememapp.models import Memory, Author
 
-def index(request):
+def memory_list(request):
     num_memories = Memory.objects.all().count()
     num_authors = Author.objects.all().count()
+    memories = Memory.objects.all()
 
     context = {
         'num_memories': num_memories,
         'num_authors': num_authors,
+        'memories': memories,
     }
 
     return render(request, 'index.html', context=context)
+
+def memory_detail(request, pk):
+    memory = get_object_or_404(Memory, pk=pk)
+    return render(request, 'memory_detail.html', {'memory': memory})
 
 def rjc(request):
     return render(request, 'rjc.html')
 
 def klc(request):
     return render(request, 'klc.html')
+
+def lac(request):
+    memories = Memory.objects.all()
+    return render(request, 'lac.html', {'memories': memories})
